6447c6c4d2a37c27650255ab2ce196f3c28988fe
songahh <gunsong2@gmail.com>
2022-10-07 16:57:12 +0900

    MySQL 연동 - 테스트 코드 통과

---
+++ src/main/java/com.betweenourclothes/domain/members/Members.java
@@ -0,0 +1,49 @@
+package com.betweenourclothes.domain.members;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+
+@Getter
+@DynamicInsert
+@NoArgsConstructor
+@Entity
+@Table(name = "members")
+public class Members {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length=45, nullable=false)
+    private String email;
+
+    @Column(length=15, nullable=false)
+    private String password;
+
+    @Column(length=10, nullable=false)
+    private String name;
+
+    @Column(length=20, nullable=false)
+    private String nickname;
+
+    @Column(length=11, nullable=false)
+    private String phone;
+
+    @Column(columnDefinition = "varchar(100) default 'default_img.jpg'")
+    private String image;
+
+    @Builder
+    public Members(String email, String password, String name, String nickname, String phone, String image){
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.name = name;
+        this.phone = phone;
+        this.image = image;
+    }
+}
---
+++ src/test/java/com.betweenourclothes/domain/members/MembersRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.betweenourclothes.domain.members;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MembersRepositoryTest {
+
+    @Autowired
+    MembersRepository membersRepository;
+
+    //@After
+    /*public void cleanup(){
+        membersRepository.deleteAll();
+    }*/
+
+    @Test
+    public void 멤버_등록_불러오기(){
+        //given
+        String email = "ddd@naver.com";
+        String password = "1234";
+        String name = "권송아";
+        String nickname = "닉넴";
+        String phone = "01012345678";
+
+        membersRepository.save(Members.builder()
+                .email(email)
+                .password(password)
+                .name(name)
+                .nickname(nickname)
+                .phone(phone).build());
+
+        List<Members> membersList = membersRepository.findAll();
+
+        Members member = membersList.get(membersList.size()-1);
+        assertThat(member.getEmail()).isEqualTo(email);
+    }
+
+}
